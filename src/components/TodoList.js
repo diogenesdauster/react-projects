9193c698e7d5c2caff0160e7a126f1e84d2e85ec
disias <diogenes.dauster@gmail.com>
2018-06-22 17:14:30 -0300

    Bug fixed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import Todo from "./Todo";
 
-const TodoList = ({ todos, onTodoClick }) => {
+const TodoList = ({ todos, onTodoClick }) => (
   <ul>
     {todos.map((todo, index) => (
       <Todo key={index} {...todo} onClick={() => onTodoClick(index)} />
     ))}
-  </ul>;
-};
+  </ul>
+);
 
 TodoList.PropTypes = {
   todos: PropTypes.arrayOf(
