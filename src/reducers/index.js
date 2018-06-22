f714e088f22884f49f7886046f52f77c1326ab67
disias <diogenes.dauster@gmail.com>
2018-06-22 15:56:25 -0300

    creating recuders, but with composition

---
+++ src/reducers/index.js
@@ -0,0 +1,60 @@
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  VisibilityFilters,
+  SET_VISIBILITY_FILTER
+} from '../actions/index';
+
+const { SHOW_ALL } = VisibilityFilters;
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ];
+    case TOGGLE_TODO: {
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          });
+        }
+      });
+    }
+    default:
+      return state;
+  }
+}
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+function todoApp(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      });
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      });
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      });
+    default:
+      return state;
+  }
+}
