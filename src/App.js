15adc6cc29988b942c5165e0f5ba30c0d99737c6
disias <diogenes.dauster@gmail.com>
2018-06-22 21:30:13 -0300

    Adding the componentes in the index.js and correct some bug in VisibleTodoList

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { createStore } from "redux";
-import todoApp from "./reducers/index";
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from "./actions/index";
-
-const store = createStore(todoApp);
-
-console.log(store.getState());
-
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(addTodo("Learn about actions"));
-store.dispatch(addTodo("Learn about reducers"));
-store.dispatch(addTodo("Learn about store"));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-
-unsubscribe();
+import Footer from "./components/Footer";
+import AddTodo from "./containers/AddTodo";
+import VisibleTodoList from "./containers/VisibleTodoList";
 
 class App extends Component {
   render() {
@@ -33,9 +13,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+        <div>
+          <AddTodo />
+          <VisibleTodoList />
+          <Footer />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, compose } from "redux";
+import todoApp from "./reducers/index";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-disable no-underscore-dangle */
+const store = createStore(todoApp, composeEnhancers());
+/* eslint-enable */
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
